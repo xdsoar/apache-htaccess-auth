f002151f1fe450d9cd178670b883a7630b4b4e9d
mrunal4 <mrunal4888@gmail.com>
2017-12-16 23:40:32 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # apache-htaccess-auth
 Apache authentication via htaccess
+
+Created and Maintained by : Mrunal Nachankar "<mrunal4888@gmail.com>"
+
+---
+
+## To use the image:
+
+
+### Express way
+
+* Start new docker container. We have 2 ways to do it:
+    * Start container with docker run
+        
+            docker run --rm -d -t -i -p 80:80 -p 443:443  -v /storage/Docker/html:/var/www/html --restart=always  --name=apache-htaccess-auth  mru/apache-htaccess-auth:latest 
+
+    * Start container with docker-compose
+        
+            docker-compose up -d
+
+* Check on web browser 
+    * If port is not changed. 
+
+            http://localhost 
+            
+    * If port is changed then check on 
+
+            http://localhost:<port>     
+ 
+
+<br>
+
+<br>
+
+### Conventional way:
+
+ 1. First pull the docker image 
+
+        docker pull mru/apache-htaccess-auth:latest
+    > It will pull image layer wise. Once completed bash prompt will be released to enter new command.
+
+ 2. Check the image is listed in the images list
+
+        docker images
+    > Check for **mru/apache-htaccess-auth:latest** under **REPOSITORY** column values
+
+ 3. Now time has come to start new docker container with the pulled image. We have 2 ways to do it:
+    * Start container with docker run
+        
+            docker run --rm -d -t -i -p 80:80 -p 443:443  -v /storage/Docker/html:/var/www/html --restart=always  --name=apache-htaccess-auth  mru/apache-htaccess-auth:latest 
+
+    * Start container with docker-compose
+        
+            docker-compose up -d
+ 4. Check on web browser 
+    * If port is not changed. 
+
+            http://localhost 
+
+    * If port is changed then check on 
+
+            http://localhost:<port>     
+
+---
+
+Thank you for using it. In case of any queries or suggestion feel fre to write back on mrunal4888@gmail.com.
+
+---
---
+++ create-htaccess-users.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------#
+# Create all users of htaccess credentials from csv provided / username and password provided 
+# File name    : create-htaccess-users.sh
+# File version : 1.0
+# Created by   : Mr. Mrunal M. Nachankar
+# Created on   : 30-08-2017 09:22:AM
+# Modified by  : None
+# Modified on  : Not yet
+# Description  : This file is used for creating the users either from csv file provided as an argument or credentials (username and password) supplied as argument. 
+#--------------------------------------------------------------------#
+
+set -e
+
+CREATE_USER_LOG_FILE="/root/create-user.log";
+#CREATE_USER_ERROR_LOG_FILE="create-user-error.log";
+
+
+echo $#;
+no_of_arguments=$#;
+echo "Number of arguments: ${no_of_arguments} ($#)";
+
+if [ ${no_of_arguments} == 1 ]; then
+
+    echo "Valid number of arguments. (${no_of_arguments})";
+    FILE_NAMES="$1";
+    if [ -f ${FILE_NAMES} ]; then
+        echo "File ${FILE_NAMES}. File exist."
+    elif [ ! -f ${FILE_NAMES} ]; then
+        echo "File ${FILE_NAMES}. File doesn't exist."
+    else
+        echo "File ${FILE_NAMES}. Oops something went wrong, please contact administrator."
+    fi
+    #FILE_NAMES=( "/root/create_users.csv" );                 # For testing purpose
+    echo "File names are: ${FILE_NAMES[@]} and Timestamp: $DateTime_STAMP2";
+    #echo "array : Len of array ${#FILE_NAMES[@]} : Len of first ${#FILE_NAMES} : Array ${FILE_NAMES[@]}"; # : Testing purpose printing
+
+    echo "File names selected for user creation: ${FILE_NAMES[@]}";
+    #echo "No of files: ${#FILE_NAMES[@]}"
+
+    while IFS=, read -r ht_uname ht_passwd
+    do
+        echo "I got:${ht_uname}|${ht_passwd}";
+        htpasswd -b /etc/apache2/.htpasswd ${ht_uname} ${ht_passwd}
+    done < ${FILE_NAMES}
+    exit
+
+elif [ ${no_of_arguments} == 2 ]; then
+
+    echo "Valid number of arguments. (${no_of_arguments})";
+    ht_uname=$1;
+    ht_passwd=$2; 
+    echo "I got:${ht_uname}|${ht_passwd}";
+    
+    htpasswd -b /etc/apache2/.htpasswd ${ht_uname} ${ht_passwd}
+    exit
+
+else
+
+    echo "Invalid no of arguments. (${no_of_arguments})";
+    echo "Hence exiting. Please try again later.";
+    exit
+
+fi
+
+
+#create_users_function |   tee  ${CREATE_USER_LOG_FILE};
+exit
